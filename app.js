f6ffb14b6683adef8c579a68b4356de368e8f537
John Lee <john_lee@ahm.honda.com>
2017-08-01 17:23:11 -0700

    add pages

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const exphbs = require('express-handlebars')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index', {title: 'main page'})
 })
 
 app.get('/about', function (req, res) {
@@ -18,20 +17,20 @@ app.get('/about', function (req, res) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
